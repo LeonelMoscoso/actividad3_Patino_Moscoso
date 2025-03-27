727bdd4a9649fa93ec99dc8be7c7ecf4769be93d
Patino_Moscoso <leonel.moscoso@est.iudigital.edu.co>
2025-03-27 18:47:04 -0500

    dataFrame

--- actividad_3.py
+++ actividad_3.py
@@ -77,5 +77,33 @@ utensilios = pd.Series(
 # Mostrar la serie
 print(utensilios)
 
-# Guardar la serie en un archivo CSV
-utensilios.to_csv('utensilios.csv', header=True, index=True)
+# Cargar el archivo CSV (asegúrate de especificar la ruta correcta)
+# Cambia 'ruta/al/archivo.csv' por el nombre de tu archivo
+df = pd.read_csv('C:\winemag-data-130k-v2.csv')
+
+# Mostrar las primeras filas del DataFrame
+print(df.head())
+
+# Para visualizar todas las columnas y configurar opciones
+pd.set_option('display.max_columns', None)  # Muestra todas las columnas
+pd.set_option('display.width', 1000)       # Ajuste de ancho para mejor visualización
+
+# Mostramos el DataFrame completo (si no es muy grande)
+print(df)
+
+# Cargar los datos en un DataFrame (cambia 'ruta/a/tu/archivo.csv' con la ruta de tu archivo)
+df = pd.read_csv('C:\winemag-data-130k-v2.csv')
+
+# Obtener las primeras cinco filas
+primeras_filas = df.head(5)
+
+# Obtener las últimas cinco filas
+ultimas_filas = df.tail(5)
+
+# Concatenar las dos partes verticalmente
+resultados = pd.concat([primeras_filas, ultimas_filas])
+
+# Guardar el resultado en un archivo CSV
+resultados.to_csv('resultado_primeras_ultimas_filas.csv', index=False)
+
+print("Archivo generado: resultado_primeras_ultimas_filas.csv")
